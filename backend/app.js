56783dc776dd6b3ed4b5e40e73f944209164220e
amilpa <amilpa2020@gmail.com>
2025-04-10 21:00:54 +0530

    remove deprecated options

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const cron = require("node-cron");
 const axios = require("axios"); // Changed from node-fetch to axios
@@ -18,10 +17,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(process.env.MONGO_URI);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
